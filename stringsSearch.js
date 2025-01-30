ea84a742c8fe3cf91988aabf120397894cd41b9d
Isaac Alexander <Isaacalexander81@gmail.com>
2025-01-30 02:30:09 +0100

    stringsSearch.js

--- stringsSearch.js
+++ stringsSearch.js
@@ -14,9 +14,9 @@
 // The indexOf() method returns the index (position) of the first occurrence of a string in a string, or it returns -1 if the string is not found:
 
 // Example
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.indexOf("locate");
-// console.log(index); //7
+let text$ = "Please locate where 'locate' occurs!";
+let index$ = text$.indexOf("locate");
+console.log(index$); //7
 
 // Note
 // JavaScript counts positions from zero.
@@ -28,45 +28,44 @@
 // The lastIndexOf() method returns the index of the last occurrence of a specified text in a string:
 
 // Example
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.lastIndexOf("locate");
-// console.log(index); //21
+const text1 = "Please locate where 'locate' occurs!";
+const index1 = text1.lastIndexOf("locate");
+console.log(index1); //21
 
 // ////////////////////////////////////////////////////////////////////
 // Both indexOf(), and lastIndexOf() return -1 if the text is not found:
 
 // Example
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.indexOf("John")
-// console.log(index); //-1
+const text2 = "Please locate where 'locate' occurs!";
+const index2 = text2.indexOf("John")
+console.log(index2); //-1
 
 // Both methods accept a second parameter as the starting position for the search:
 
 // Example
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.indexOf("locate",15);
-// console.log(index); //21
+const text3 = "Please locate where 'locate' occurs!";
+const index3 = text3.indexOf("locate", 15);
+console.log(index3); //21
 
 // The lastIndexOf() methods searches backwards (from the end to the beginning), meaning: if the second parameter is 15, the search starts at position 15, and searches to the beginning of the string.
 
 // Example
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.lastIndexOf("locate", 15);
-// console.log(index); //7
+const text4 = "Please locate where 'locate' occurs!";
+const index4 = text4.lastIndexOf("locate", 15);
+console.log(index4); //7
 
 ///////////////////////////////////////////////////////////////////
 // JavaScript String search()
 // The search() method searches a string for a string (or a regular expression) and returns the position of the match:
 
-// // Examples
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.search("locate");
-// console.log(index); //7
-
+// Examples
+const text5 = "Please locate where 'locate' occurs!";
+const index5 = text5.search("locate");
+console.log(index5); //7
 /////////////////////////////////////////////////////////
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.search(/locate/);
-// console.log(index); //7
+const text6 = "Please locate where 'locate' occurs!";
+const index6 = text6.search(/locate/);
+console.log(index6); //7
 
 // Did You Notice?
 // The two methods, indexOf() and search(), are equal?
@@ -86,31 +85,30 @@
 // Examples
 // Perform a search for "ain":
 
-// let text = "The rain in SPAIN stays mainly in the plain";
-// const myArr = text.match("ain");
-// console.log(myArr.length + " " + myArr);  //1 ain
+const text1$ = "The rain in SPAIN stays mainly in the plain";
+const myArr1$ = text1$.match("ain");
+console.log(myArr1$.length + " " + myArr1$);  //1 ain
 
 /////////////////////////////////////////////////////////////
 //Perform a search for "ain":
 
-// let text = "The rain in SPAIN stays mainly in the plain"; 
-// const myArr = text.match(/ain/);
-// console.log(myArr.length + " " + myArr); //1 ain
+const text2$ = "The rain in SPAIN stays mainly in the plain";
+const myArr2$ = text2$.match(/ain/);
+console.log(myArr2$.length + " " + myArr2$); //1 ain
 
 /////////////////////////////////////////////////////
 //Perform a global search for "ain":
 
-// let text = "The rain in SPAIN stays mainly in the plain"; 
-// const myArr = text.match(/ain/g);
-// console.log(myArr.length + " " + myArr); //3 ain,ain,ain
+const text3$ = "The rain in SPAIN stays mainly in the plain";
+const myArr3$ = text3$.match(/ain/g);
+console.log(myArr3$.length + " " + myArr3$); //3 ain,ain,ain
 
 /////////////////////////////////////////////////////////
 // Perform a global, case-insensitive search for "ain":
 
-// let text = "The rain in SPAIN stays mainly in the plain"; 
-// const myArr = text.match(/ain/gi);
-// console.log(myArr.length + " " + myArr); //4 ain,AIN,ain,ain
-
+const text4$ = "The rain in SPAIN stays mainly in the plain";
+const myArr4$ = text4$.match(/ain/gi);
+console.log(myArr4$.length + " " + myArr4$); //4 ain,AIN,ain,ain
 // Note
 // If a regular expression does not include the g modifier (global search), match() will return only the first match in the string.
 
@@ -119,12 +117,20 @@
 // The matchAll() method returns an iterator containing the results of matching a string against a string (or a regular expression).
 
 // Example
-// let text = "I love cats. Cats are very easy to love. Cats are very popular."
-// const iterator = text.matchAll("Cats");
-// console.log(Array.from(iterator)); //Cats,Cats*
+const text7 = "I love cats. Cats are very easy to love. Cats are very popular."
+const iterator1 = text7.matchAll("Cats");
+console.log(Array.from(iterator1)); //Cats,Cats*
 
 //////////////////////////////////////////////////////////////
 //If the parameter is a regular expression, the global flag (g) must be set, otherwise a TypeError is thrown.
 //Example
 
+const text8 = "I love cats. Cats are very easy to love. Cats are very popular."
+const iterator2 = text8.matchAll(/Cats/g);
+console.log(Array.from(iterator2));
 
+// If you want to search case insensitive, the insensitive flag (i) must be set:
+
+const text9 = "I love cats. Cats are very easy to love. Cats are very popular."
+const iterator3 = text9.matchAll(/Cats/gi);
+console.log(Array.from(iterator3));
