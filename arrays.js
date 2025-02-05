1d2b72bcc809e3f5174d0bf1ed22f15080bd1db3
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-05 01:59:34 +0100

    arrays.js

--- arrays.js
+++ arrays.js
@@ -343,24 +343,25 @@ console.log("Array: " + (fruits instanceof Array)); //Array: true
 // Values in objects can be arrays, and values in arrays can be objects:
 
 // Example
-// const myObj = {
-//   name: "John",
-//   age: 30,
-//   cars: [
-//     {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
-//     {name:"BMW", models:["320", "X3", "X5"]},
-//     {name:"Fiat", models:["500", "Panda"]}
-//   ]
-// }
-// // To access arrays inside arrays, use a for-in loop for each array:
+let x = "";
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]},
+    {name:"Fiat", models:["500", "Panda"]}
+  ]
+}
+// To access arrays inside arrays, use a for-in loop for each array:
 
 
-// for (let i in myObj.cars) {
-//     x += " " + myObj.cars[i].name + " ";
-//     for (let j in myObj.cars[i].models) {
-//       x += myObj.cars[i].models[j] + " ";
-//     }
-//   }
+for (let i in myObj.cars) {
+    x += " " + myObj.cars[i].name + " ";
+    for (let j in myObj.cars[i].models) {
+      x += myObj.cars[i].models[j] + " ";
+    }
+  }
 
-//   console.log(x);
+  console.log(x);
   
