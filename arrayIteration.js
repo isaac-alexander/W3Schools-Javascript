063f2e4b5ba8f9559765230ac8a319abc40b0f8f
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-20 02:50:18 +0100

    arrayIteration.js

--- arrayIteration.js
+++ arrayIteration.js
@@ -59,7 +59,7 @@
 const numbers1 = [45, 4, 9, 16, 25];
 const numbers2 = numbers1.map(myFunction);
 function myFunction(value, index, array) {
-  return value * 2;
+    return value * 2;
 }
 console.log(numbers2); //[ 90, 8, 18, 32, 50 ]
 
@@ -73,6 +73,149 @@ console.log(numbers2); //[ 90, 8, 18, 32, 50 ]
 const numbers1a = [45, 4, 9, 16, 25];
 const numbers2a = numbers1.map(myFunction);
 function myFunction(value) {
-  return value * 2;
+    return value * 2;
 }
 console.log(numbers2); //[ 90, 8, 18, 32, 50 ]
+
+//////////////////////////////////////////////////////////////////////
+// JavaScript Array flatMap()
+// ES2019 added the Array flatMap() method to JavaScript.
+
+// The flatMap() method first maps all elements of an array and then creates a new array by flattening the array.
+
+// Example
+const myArr = [1, 2, 3, 4, 5, 6];
+const newArr = myArr.flatMap(x => [x, x * 10]);
+console.log(newArr);   //[ 1, 10,  2, 20,  3, 30,  4, 40,  5, 50, 6, 60 ]
+
+///////////////////////////////////////////////////////////////////////
+// JavaScript Array filter()
+// The filter() method creates a new array with array elements that pass a test.
+
+// This example creates a new array from elements with a value larger than 18:
+
+// Example
+
+const numbers = [45, 4, 9, 16, 25];
+const over18 = numbers.filter(myFunction);
+function myFunction(value, index, array) {
+    return value > 18;
+}
+console.log(over18);  //[ 45, 25 ]
+
+// Note that the function takes 3 arguments:
+
+// The item value
+// The item index
+// The array itself
+// In the example above, the callback function does not use the index and array parameters, so they can be omitted:
+
+// Example
+const numbersA = [45, 4, 9, 16, 25];
+const over18A = numbersA.filter(myFunction);
+
+function myFunction(value) {
+    return value > 18;
+}
+console.log(over18A); //[ 45, 25 ]
+
+////////////////////////////////////////////////////////////////////
+// JavaScript Array reduce()
+// The reduce() method runs a function on each array element to produce (reduce it to) a single value.
+
+// The reduce() method works from left-to-right in the array. See also reduceRight().
+
+// The reduce() method does not reduce the original array.
+
+// This example finds the sum of all numbers in an array:
+
+// Example
+const numbersB = [45, 4, 9, 16, 25];
+let sum = numbersB.reduce(myFunction);
+
+function myFunction(total, value, index, array) {
+  return total + value;
+}
+console.log("The sum is " + sum); //The sum is 99
+
+// Note that the function takes 4 arguments:
+
+// The total (the initial value / previously returned value)
+// The item value
+// The item index
+// The array itself
+// The example above does not use the index and array parameters. It can be rewritten to:
+
+// Example
+const numbersC = [45, 4, 9, 16, 25];
+let sumC = numbersC.reduce(myFunction);
+
+function myFunction(total, value) {
+  return total + value;
+}
+console.log("The sum is " + sumC); //The sum is 99
+
+//The reduce() method can accept an initial value:
+
+// Example
+const numbersD = [45, 4, 9, 16, 25];
+let sumD = numbersD.reduce(myFunction, 100);
+
+function myFunction(total, value) {
+  return total + value;
+}
+console.log("The sum is " + sumD); //The sum is 199
+
+///////////////////////////////////////////////////////////////////////////
+
+// JavaScript Array reduceRight()
+// The reduceRight() method runs a function on each array element to produce (reduce it to) a single value.
+
+// The reduceRight() works from right-to-left in the array. See also reduce().
+
+// The reduceRight() method does not reduce the original array.
+
+// This example finds the sum of all numbers in an array:
+
+// Example
+const numbersE = [45, 4, 9, 16, 25];
+let sumE = numbersE.reduceRight(myFunction);
+
+function myFunction(total, value, index, array) {
+  return total + value;
+}
+console.log( "The sum is " + sumE); //The sum is 99
+
+// Note that the function takes 4 arguments:
+
+// The total (the initial value / previously returned value)
+// The item value
+// The item index
+// The array itself
+// The example above does not use the index and array parameters. It can be rewritten to:
+
+// Example
+
+const numbersF = [45, 4, 9, 16, 25];
+let sumF = numbersF.reduceRight(myFunction);
+
+function myFunction(total, value) {
+  return total + value;
+}
+console.log("The sum is " + sumE); //The sum is 99
+
+/////////////////////////////////////////////////////////////////////
+// JavaScript Array every()
+// The every() method checks if all array values pass a test.
+
+// This example checks if all array values are larger than 18:
+
+// Example
+
+const numbersG = [45, 4, 9, 16, 25];
+let allOver18 = numbersG.every(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+console.log("All over 18 is " + allOver18); //All over 18 is false
